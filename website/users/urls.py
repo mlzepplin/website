b14720a3314f9b5a0c11f8454225be9bab7b0a01
mlzepplin <rishabh.gupta.1606@gmail.com>
2024-09-30 23:57:56 -0700

    add django login support

--- website/users/urls.py
+++ website/users/urls.py
@@ -1,8 +1,9 @@
 # users/users.py
 
-from django.urls import path
+from django.urls import path, include
 from users.views import dashboard
 
 urlpatterns = [
     path("dashboard/", dashboard, name="dashboard"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
